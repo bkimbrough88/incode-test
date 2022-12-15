4f00734252fe03f5b9d041ec93f609f3c96d60a3
Brandon Kimbrough <brandon.kimbrough@onna.com>
2022-12-15 01:44:49 -0500

    more complete front end code with dockerfile

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,38 +2,99 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import {Grid} from "@mui/material";
-import Post from "./Post";
+import {Button, Grid, TextField} from '@mui/material';
+import Post, {PostAttributes} from './Post';
+import {v4 as uuidv4} from 'uuid';
 
 function Copyright() {
-  return (
-      <Typography variant="body2" color="text.secondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://mui.com/">
-          Brandon Kimbrough
-        </Link>{' '}
-        {new Date().getFullYear()}.
-      </Typography>
-  );
+    return (
+        <Typography variant='body2' color='text.secondary' align='center'>
+            {'Copyright © '}
+            <Link color='inherit' href='https://mui.com/'>
+                Brandon Kimbrough
+            </Link>{' '}
+            {new Date().getFullYear()}.
+        </Typography>
+    );
 }
 
-const posts = [
-    {
-        id: "abc",
-        datePosted: "2022-12-13:15:20:36Z",
-        author: "Bob",
-        message: "Hello World"
+interface appState {
+    dataIsLoaded: boolean;
+    posts: PostAttributes[];
+    author: string;
+    message: string;
+}
+
+class App extends React.Component<any, appState> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            dataIsLoaded: false,
+            posts: [],
+            author: '',
+            message: ''
+        }
     }
-]
 
-export default function App() {
-  return (
-      <Container maxWidth="sm">
-        <Grid container spacing={4}>
-            {posts.map((post) => (
-                <Post key={post.id} post={post}/>
-            ))}
-        </Grid>
-      </Container>
-  );
+    componentDidMount() {
+        fetch(`/api/posts`)
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    dataIsLoaded: true,
+                    posts: json
+                })
+            })
+    }
+
+    handleAuthorChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.setState({author: e.target.value})
+    }
+
+    handleMessageChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.setState({message: e.target.value})
+    }
+
+    handleSubmitButton() {
+        const post: PostAttributes = {
+            id: uuidv4(),
+            datePosted: Date.now().toString(),
+            author: this.state.author,
+            message: this.state.message
+        }
+        fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify(post),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            if (!res.ok) {
+                console.log(`Failed to add message: ${res.statusText}`)
+            }
+        })
+    }
+
+    render() {
+        const { dataIsLoaded, posts } = this.state;
+        if (!dataIsLoaded)
+            return <Typography component='h1'>Loading....</Typography>
+
+        return (
+            <Container maxWidth='sm'>
+                <TextField id='author' label='author' onChange={this.handleAuthorChange} variant='outlined' />
+                <TextField id='message' label='message' onChange={this.handleMessageChange} variant='outlined' />
+                <Button id='submit' title='Add Comment' onSubmit={this.handleSubmitButton} variant='contained' />
+                <Grid container spacing={4}>
+                    {posts.map((post) => (
+                        <Post key={post.id} post={post}/>
+                    ))}
+                </Grid>
+                <Copyright/>
+            </Container>
+        );
+    }
 }
+
+export default App;
--- front/src/Post.tsx
+++ front/src/Post.tsx
@@ -4,33 +4,37 @@ import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
+export interface PostAttributes {
+    id: string;
+    author: string;
+    datePosted: string;
+    message: string;
+}
+
 interface PostProperties {
-    post: {
-        id: string;
-        author: string;
-        datePosted: string;
-        message: string;
+    post : PostAttributes
+}
+
+class Post extends React.Component<PostProperties, any> {
+    render() {
+        return (
+            <Grid item>
+                <Card sx={{ display: 'flex' }}>
+                    <CardContent sx={{ flex: 1 }}>
+                        <Typography component="h2" variant="h5">
+                            {this.props.post.author}
+                        </Typography>
+                        <Typography variant="subtitle1" color="text.secondary">
+                            {this.props.post.datePosted}
+                        </Typography>
+                        <Typography variant="subtitle1" paragraph>
+                            {this.props.post.message}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        );
     }
 }
 
-export default function (props: PostProperties) {
-    const { post } = props
-
-    return (
-        <Grid item>
-            <Card sx={{ display: 'flex' }}>
-                <CardContent sx={{ flex: 1 }}>
-                    <Typography component="h2" variant="h5">
-                        {post.author}
-                    </Typography>
-                    <Typography variant="subtitle1" color="text.secondary">
-                        {post.datePosted}
-                    </Typography>
-                    <Typography variant="subtitle1" paragraph>
-                        {post.message}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Grid>
-    );
-}
+export default Post;
